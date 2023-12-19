b1b1be8af8f8686cd8ce631f8df8bf1bc0fcf299
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2023-12-20 00:03:05 +0100

    Setup Metamask

---
+++ Games/Poker/addons/metamask/usage.md
@@ -0,0 +1,296 @@
+# Metamask
+Quick links
+- [Functions](#functions)
+- [Signals](#signals)
+- [Project Settings](#project-settings)
+
+
+## Functions
+### Is Metamask Installed
+Checks if client has Metamask installed
+
+NOTE - This is not 100% accurate.  Because it is checking a JS property, this can be faked by another wallet provider.
+
+See [the Metamask docs](https://docs.metamask.io/guide/ethereum-provider.html#ethereum-isconnected) for details
+```
+func has_metamask() -> bool
+```
+
+
+### Is Network Connected
+Checks if Metamask can talk to the current chain.  Returns false when the user is having network connectivity issues or the chain is experiencing difficulties
+```
+func is_network_connected() -> bool
+```
+
+
+### Current Chain
+Gets the current connected chain id 
+```
+func current_chain() -> String
+```
+
+
+### Selected Account
+Gets the active account address or null if no account has connected to the Application
+```
+func selected_account() -> String || null
+```
+
+
+### Request Accounts
+Requests permission to view user's accounts. Fires request_accounts_finished when complete.
+
+Currently only returns the active account in Metamask, but passes back an Array for future proofing
+
+Result is returned by [request_accounts_finished](#request-accounts-finished) signal
+```
+func request_accounts()
+```
+
+
+### Switch to Chain
+Sends notification to user to switch to the chain with the provided ID.
+
+See https://chainlist.org/ for list of chains and their details
+
+Result is returned by [switch_to_chain_finished](#switch-to-chain-finished) signal
+```
+# chain_id: String - ID of chain to connect to
+func switch_to_chain(chain_id: String)
+```
+
+
+### Client Version
+Gets the version of Metamask currently running.
+
+Result is returned by [client_version_finished](#client-version-finished) signal
+```
+func client_version()
+```
+
+
+### Wallet Balance
+Get the balance of wallet at address in wei
+
+Result is returned by [wallet_balance_finished](#wallet-balance-finished) signal
+```
+# address: String - Address of wallet to retrieve ETH balance from
+func wallet_balance(address: String)
+```
+
+
+### Token Balance
+Get the balance of an ERC20 token for an address
+
+Result is returned by [wallet_balance_finished](#wallet-balance-finished) signal
+```
+# token_address: String - Address of the Smart Contract for the ERC20 token
+# address: String - Address to retrieve balance against
+func token_balance(token_address: String, address: String)
+```
+
+
+### Add Ethereum Chain
+Add a custom ethereum-based chain to your wallet. Examples of ethereum-based chains are [Polygon](https://polygon.technology/) and [Binance](https://www.binance.org/en)
+
+See https://chainlist.org/ for list of chains and their details
+
+Result is returned by [add_eth_chain_finished](#add-ethereum-chain-finished) signal
+```
+# chain_id: String - ID of chain to connect to
+# chain_name: String - Name to show for the chain
+# rpc_url: String - URL to send RPC requests to
+# currency_symbol: Optional[String] - 2-6 character symbol to refer to the token used by the chain
+# block_explorer_url: Optional[String] - URL of website to browse block information for the chain
+func add_eth_chain(chain_id: String, chain_name: String, rpc_url: String, currency_symbol = null, block_explorer_url = null)
+```
+
+
+### Add Custom Token
+Requests Metamask to track a specified ERC20 token in the connected account
+
+Result is returned by [add_custom_token_finished](#add-custom-token-finished) signal
+```
+# token_address: String - Address of the Smart Contract for the ERC20 token
+# token_symbol: String - 2-6 character symbol to refer to the token
+# image_url: String - URL for image to use for the token
+func add_custom_token(token_address: String, token_symbol: String, image_url: String)
+```
+
+### Current Gas Price
+Get the current gas price in Wei
+
+Result is returned by [current_gas_price_finished](#current-gas-price-finished) signal
+```
+func current_gas_price()
+```
+
+
+### Send Token
+Send some amount of an ERC20 token from one account to another.
+
+NOTE - Most ERC20 tokens will require the from_address to be the sender of the request.
+In most use cases you will use `from_address = Metamask.selected_account()`
+
+Result is returned by [send_token_finished](#send-token-finished) signal
+```
+# from_address: String - Wallet where the tokens will be sent from
+# recipient_address: String - Wallet that will receive the tokens
+# token_address: String - Address of the Smart Contract for the ERC20 token
+# amount: float - Amount of the token to send.  Unit is "eth" (1e18)
+# gas_limit: Optional[int] - Limit on Gas to spend on the transaction
+# gas_price: Optional[float] - Price to pay per Gas unit.  Unit is "gwei" (1e9)
+func send_token(from_address: String, recipient_address: String, token_address: String, amount: float, gas_limit = null, gas_price = null)
+```
+
+
+### Send Ethereum
+Send some amount of ETH from one account to another
+
+Result is returned by [send_eth_finished](#send-ethereum-finished) signal
+```
+# from_address: String - Wallet where the ETH will be sent from
+# recipient_address: String - Wallet that will receive the ETH
+# amount: float - Amount of the token to sell.  Unit is "eth" (1e18)
+# gas_limit: Optional[int] - Limit on Gas to spend on the transaction.  Defaults to 21000, which is the constant gas needed to transfer eth between accounts
+# gas_price: Optional[float] - Price to pay per Gas unit.  Unit is "gwei" (1e9)
+func send_eth(from_address: String, recipient_address: String, amount: float, gas_limit: int = 21000, gas_price = null)
+```
+
+
+## Signals
+
+All `<function>_finished` signals share the same Response data structure
+```
+Response[T] = Dictionary
+{
+  'result': T || null, # If the call succeeded, the data returned will be passed back here. If the call failed, it will be null
+  'error': {
+	'code': Int, # Code relating to the error 
+	'message': String # Human readable string relating to the error
+  } || null # If the call failed, error will be a Dictionary containing info about the error.  If the call succeeded, it will be null
+}
+```
+
+### Request Accounts Finished
+Signal to get result of [request_accounts](#request-user-accounts) call
+```
+# response: Response[Array[String]] - Result is an array of addresses that the wallet now has permission to use
+# NOTE - At this moment, this will always be of size 0 or 1
+signal request_accounts_finished(response)
+```
+
+
+### Switch to Chain Finished
+Signal to get result of [switch_to_chain](#switch-to-chain) call
+```
+# response: Response[null] - Result is null if call succeeded
+signal switch_chain_finished(response)
+```
+
+
+### Client Version Finished
+Signal to get result of [client_version](#client-version) call
+```
+# response: Response[String] - Result is a string of the client's version
+signal client_version_finished(response)
+```
+
+
+### Wallet Balance Finished
+Signal to get result of [wallet_balance](#wallet-balance) call
+```
+# response: Response[Int] - Result is the integer value of wallet balance in wei
+signal wallet_balance_finished(response) 
+```
+
+
+### Token Balance Finished
+Signal to get result of [token_balance](#token-balance) call
+```
+# response: Response[Int] - Result is the integer value of the token associated to the address
+signal token_balance_finished(response)
+```
+
+
+### Add Ethereum Chain Finished
+Signal to get result of [add_eth_chain](#add-ethereum-chain) call
+```
+# response: Response[null] - Result is null if call succeeded
+signal add_eth_chain(response)
+```
+
+
+### Add Custom Token Finished
+Signal to get result of [add_custom_token](#add-custom-token) call
+```
+# response: Response[Int] - Result is null if call succeeded
+signal add_custom_token_finished(response)
+```
+
+
+### Current Gas Price Finished
+Signal to get result of [current_gas_price](#current-gas-price) call
+```
+# response: Response[int] - Result is the integer value of current gas price in wei
+signal current_gas_price_finished(response)
+```
+
+
+### Send Token Finished
+Signal to get result of [send_token](#send-token) call
+```
+# response: Response[String] - Result is a hex encoded transaction hash
+signal send_token_finished(response)
+```
+
+
+### Send Ethereum Finished
+Signal to get result of [send_eth](#send-ethereum) call
+```
+# response: Response[String] - Result is a hex encoded transaction hash
+signal send_eth_finished(response)
+```
+
+
+### New Account Selected
+Signal when user changes their active accounts.
+
+Currently only one account is active at a time but can potentially be more in the future
+```
+# new_accounts: Array[String] - Array of accounts user has changed to AND Application has permissions to see
+signal accounts_changed(new_accounts)
+```
+
+
+### New Chain Selected
+Signal when user changes their connected chain.
+
+See https://chainlist.org/ for list of chains and their details
+```
+# new_chain_id: String - String corresponding to the new Chain's ID
+signal chain_changed(new_chain_id)
+```
+
+
+### Chain Connected
+Signal when Metamask provider has connected to the active chain
+```
+# chain_id: String - Id of chain Metamask has connected to
+signal chain_connected(chain_id)
+```
+
+
+### Chain Disconnected
+Signal when Metamask disconnects from the active chain
+```
+# error: Dict[String, Any] - Contains "code" and "message" fields describing the error that occurred
+signal chain_disconnected(error)
+```
+
+
+# Project Settings
+|Name|Type|Description|Default|
+|---|---|---|---|
+|plugins/metamask/use_application_icon|Bool|Whether Metamask should use your Application's Icon in the Connect Popup|False|
---
+++ Games/Poker/scenes/test/TestScene.gd
@@ -0,0 +1,257 @@
+extends Control
+
+# System Checks
+@onready var installed_check_box: CheckBox = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/InstalledCheckBox
+@onready var connected_check_box: CheckBox = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/ConnectedCheckBox
+@onready var connect_button: Button = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/ConnectButton
+# Switching Chains
+@onready var chain_id_line_edit: LineEdit = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/ChainIdLineEdit
+@onready var switch_chain_button: Button = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/SwitchChainButton
+# Eth Balance
+@onready var balance_line_edit: LineEdit = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/BalanceLineEdit
+@onready var balance_button: Button = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/BalanceButton
+# Token Balance
+@onready var token_balance_line_edit: LineEdit = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/TokenBalanceAddressLineEdit
+@onready var wallet_token_line_edit: LineEdit = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/WalletTokenBalanceLineEdit
+@onready var token_balance_button: Button = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/TokenBalanceButton
+# Custom Chain
+@onready var add_binance_button: Button = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/AddBinanceButton
+@onready var add_matic_button: Button = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/AddMaticButton
+# Custom Token
+@onready var add_enjin_token_button: Button = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/AddEnjinTokenButton
+# Gas Price
+@onready var get_gas_button: Button = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/GetGasButton
+# Transfer ERC20
+@onready var transfer_from_line_edit: LineEdit = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/TransferFromLineEdit
+@onready var transfer_token_line_edit: LineEdit = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/TransferTokenLineEdit
+@onready var transfer_to_line_edit: LineEdit = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/TransferToLineEdit
+@onready var transfer_button: Button = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/TransferButton
+@onready var transfer_amount_line_edit: LineEdit = $PanelSplitContainer/UserPanelContainer/MarginContainer/UserContainer/TransferAmountLineEdit
+# Output
+@onready var output_text_edit: TextEdit = $PanelSplitContainer/OutputPanelContainer/MarginContainer/VBoxContainer/OutputTextEdit
+
+func _ready():
+	# First check if Metamask is installed
+	var installed = Metamask.has_metamask()
+	#installed_check_box.pressed = installed
+	if not installed:
+		_print("Metamask is not installed...")
+		_fail_init()
+		return
+	_print("Metamask is installed...")
+	# Check if Metamask is connected
+	var connected = Metamask.is_network_connected()
+	#connected_check_box.pressed = connected
+	if not connected:
+		_print("Metamask is not connected...")
+		_fail_init()
+		return
+	_print("Metamask is connected...")
+	# Get information on Metamask connection
+	_print("Current Chain: " + Metamask.current_chain())
+	_print("Connected Account: " + str(Metamask.selected_account()))
+	get_client_version()
+	# Connect signals
+# warning-ignore:return_value_discarded
+	Metamask.connect("request_accounts_finished", _on_Metamask_request_accounts_finished)
+# warning-ignore:return_value_discarded
+	Metamask.connect("accounts_changed", _on_Metamask_accounts_changed)
+# warning-ignore:return_value_discarded
+	Metamask.connect("chain_changed", _on_Metamask_chain_changed)
+# warning-ignore:return_value_discarded
+	Metamask.connect("switch_chain_finished", _on_Metamask_switch_chain_finished)
+# warning-ignore:return_value_discarded
+	Metamask.connect("chain_connected", _on_Metamask_chain_connected)
+# warning-ignore:return_value_discarded
+	Metamask.connect("chain_disconnected", _on_Metamask_chain_disconnected)
+# warning-ignore:return_value_discarded
+	Metamask.connect("message_received", _on_Metamask_message_received)
+# warning-ignore:return_value_discarded
+	Metamask.connect("wallet_balance_finished", _on_Metamask_wallet_balance_finished)
+# warning-ignore:return_value_discarded
+	Metamask.connect("token_balance_finished", _on_Metamask_token_balance_finished)
+# warning-ignore:return_value_discarded
+	Metamask.connect("add_eth_chain_finished", _on_Metamask_add_eth_chain_finished)
+# warning-ignore:return_value_discarded
+	Metamask.connect("add_custom_token_finished", _on_Metamask_add_custom_token_finished)
+# warning-ignore:return_value_discarded
+	Metamask.connect("send_token_finished", _on_Metamask_send_token_finished)
+
+func _print(text: String):
+	output_text_edit.text += text + "\n"
+
+func _fail_init():
+	_print("Aborting remaining checks")
+	connect_button.disabled = true
+
+# Example of inline handling of the signal from an RPC call
+func get_client_version():
+	Metamask.client_version()
+	var response = await Metamask.client_version_finished
+	if response.error != null:
+		# The call failed
+		_print("Client Version Request Failed...")
+		_print("Reason: " + response.error.message)
+		return
+	# The call succeeded
+	_print("Client Version: " + response.result)
+
+func _on_ConnectButton_pressed():
+	_print("Attempting Request Accounts")
+	connect_button.disabled = true
+	Metamask.request_accounts()
+
+func _on_Metamask_request_accounts_finished(response):
+	connect_button.disabled = false
+	if response.error != null:
+		# The call failed
+		_print("Accounts Request Failed...")
+		_print("Reason: " + response.error.message)
+		return
+	# The call succeeded
+	_print("Accounts Request Succeeded...")
+	_print("Addresses:")
+	for addr in response.result:
+		_print("\t" + addr)
+
+func _on_Metamask_accounts_changed(new_accounts):
+	_print("User Accounts changed to " + new_accounts[0])
+
+func _on_Metamask_chain_changed(new_chain_id):
+	_print("User Connected Chain changed to " + new_chain_id)
+
+func _on_Metamask_chain_connected(chain_id):
+	_print("Metamask connected to chain: " + chain_id)
+
+func _on_Metamask_chain_disconnected(error):
+	_print("Metamask was disconnected from current chain")
+	_print("Reason: " + error.message)
+
+func _on_Metamask_message_received(message):
+	_print("Metamask received message of type " + message.type)
+	_print("Message : " + str(message.data))
+
+func _on_SwitchChainButton_pressed():
+	var new_chain_id = chain_id_line_edit.text
+	_print("Attempting to Switch Chain to " + new_chain_id)
+	switch_chain_button.disabled = true
+	Metamask.switch_to_chain(new_chain_id)
+
+func _on_Metamask_switch_chain_finished(response):
+	switch_chain_button.disabled = false
+	if response.error != null:
+		_print("Chain Switch Failed...")
+		_print("Reason: " + response.error.message)
+		return
+	_print("Chain Switch Succeeded...")
+
+func _on_BalanceButton_pressed():
+	var addr = balance_line_edit.text
+	_print("Attempting to get balance of wallet at " + addr)
+	balance_button.disabled = true
+	Metamask.wallet_balance(addr)
+
+func _on_Metamask_wallet_balance_finished(response):
+	balance_button.disabled = false
+	if response.error != null:
+		_print("Wallet Balance Failed...")
+		_print("Reason: " + response.error.message)
+		return
+	_print("Wallet Balance Succeeded...")
+	# Whatever operations you wish to do with the balance (like aggregate wallet balances)
+	# it's better to do it all in Wei, then when you want to display it convert it to a friendlier amount
+	var wei_balance = response.result
+	var eth_balance = Metamask.convert_util.convert_wei(wei_balance, Metamask.convert_util.Wei.to_Eth)
+	_print("Wallet balance - " + str(eth_balance) + " eth")
+
+func _on_TokenBalanceButton_pressed():
+	var token_address = token_balance_line_edit.text
+	var wallet_address = wallet_token_line_edit.text
+	token_balance_button.disabled = true
+	Metamask.token_balance(token_address, wallet_address)
+
+func _on_Metamask_token_balance_finished(response):
+	token_balance_button.disabled = false
+	if response.error != null:
+		_print("Token Balance Failed...")
+		_print("Reason: " + response.error.message)
+		return
+	_print("Token Balance Succeeded...")
+	_print("Token Balance - " + str(response.result))
+
+func _on_AddBinanceButton_pressed():
+	add_binance_button.disabled = true
+	# Required variables
+	var chain_id = '0x38' # 56 in hex
+	var chain_name = 'Binance Smart Chain Mainnet'
+	var rpc_url = 'https://bsc-dataseed1.binance.org'
+	# Optional variables
+	var currency_symbol = 'BNB'
+	var blockscan_url = 'https://bscscan.com'
+	Metamask.add_eth_chain(chain_id, chain_name, rpc_url, currency_symbol, blockscan_url)
+
+func _on_AddMaticButton_pressed():
+	add_matic_button.disabled = true
+	# Required variables
+	var chain_id = '0x89' # 137 in hex
+	var chain_name = 'Matic Mainnet'
+	var rpc_url = 'https://polygon-rpc.com/'
+	# Optional variables
+	var currency_symbol = 'MATIC'
+	var blockscan_url = 'https://polygonscan.com/'
+	Metamask.add_eth_chain(chain_id, chain_name, rpc_url, currency_symbol, blockscan_url)
+
+func _on_Metamask_add_eth_chain_finished(response):
+	add_binance_button.disabled = false
+	add_matic_button.disabled = false
+	if response.error != null:
+		_print("Add Eth Chain Failed...")
+		_print("Reason: " + response.error.message)
+		return
+	_print("Add Eth Chain Succeeded...")
+
+func _on_AddEnjinTokenButton_pressed():
+	add_enjin_token_button.disabled = true
+	var token_address: String = '0xf629cbd94d3791c9250152bd8dfbdf380e2a3b9c'
+	var token_symbol: String = 'ENJ'
+	var image_url: String = 'https://cryptologos.cc/logos/enjin-coin-enj-logo.png'
+	Metamask.add_custom_token(token_address, token_symbol, image_url)
+
+func _on_Metamask_add_custom_token_finished(response):
+	add_enjin_token_button.disabled = false
+	if response.error != null:
+		_print("Add Custom Token Failed...")
+		_print("Reason: " + response.error.message)
+		return
+	_print("Add Custom Token Succeeded...")
+	_print("Add Custom Token - " + str(response.result))
+
+func _on_GetGasButton_pressed():
+	get_gas_button.disabled = true
+	Metamask.current_gas_price()
+	var response = await Metamask.current_gas_price_finished
+	if response.error != null:
+		_print("Get Gas Price Failed...")
+		_print("Reason: " + response.error.message)
+		return
+	var gwei_gas = Metamask.convert_util.convert_wei(response.result, Metamask.convert_util.Wei.to_Gwei)
+	_print("Current Gas Price = %d gwei" % gwei_gas)
+	get_gas_button.disabled = false
+	
+
+func _on_TransferButton_pressed():
+	transfer_button.disabled = true
+	var from = transfer_from_line_edit.text
+	var token = transfer_token_line_edit.text
+	var recipient = transfer_to_line_edit.text
+	var amount = int(transfer_amount_line_edit.text)
+	Metamask.send_token(from, token, recipient, amount)
+
+func _on_Metamask_send_token_finished(response):
+	transfer_button.disabled = false
+	if response.error != null:
+		_print("Send Token Failed...")
+		_print("Reason: " + response.error.message)
+		return
+	_print("Send Token Succeeded...")
+	_print("Transaction Hash - " + str(response.result))
